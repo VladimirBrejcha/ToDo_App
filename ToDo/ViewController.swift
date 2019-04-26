b3237cd45b3e453fe7a419621f8d07ab3315d02a
Vladimir Korolev <python2python@icloud.com>
2019-04-26 16:21:09 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Vladimir Korolev on 26/04/2019.
+//  Copyright © 2019 Vladimir Brejcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
