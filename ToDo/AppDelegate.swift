1d46a29aa1ea3cbe459782d6d08f3037b3af61b4
Vladimir Korolev <python2python@icloud.com>
2019-04-29 19:49:44 +0300

    search function works again

--- ToDo/AppDelegate.swift
+++ ToDo/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print(Realm.Configuration.defaultConfiguration.fileURL)
         do {
             let realm = try Realm()
         } catch {
