05ac608bee3a462673216a03bc1a0ef485b7f67c
Vladimir Korolev <python2python@icloud.com>
2019-04-29 20:18:03 +0300

    DateSorting in search now works as expected

--- ToDo/AppDelegate.swift
+++ ToDo/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+//        print(Realm.Configuration.defaultConfiguration.fileURL)
         do {
             let realm = try Realm()
         } catch {
